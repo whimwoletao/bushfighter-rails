b17ef80004e87c1c97cc856f288da7878323681f
Sulphur1010 <mustaphaalade@gmail.com>
2017-01-16 11:49:08 +0100

    fix signup bug

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Members::RegistrationsController < Devise::RegistrationsController
 layout 'signin'
-before_filter :configure_sign_up_params, only: [:create]
+# before_filter :configure_sign_up_params, only: [:create]
 before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -12,7 +12,7 @@ before_filter :configure_account_update_params, only: [:update]
   def create
     super
   end
-  
+
 
   # GET /resource/edit
   def edit
@@ -52,19 +52,19 @@ before_filter :configure_account_update_params, only: [:update]
   #   if Rails.env.production?
   #     notifier = Slack::Notifier.new "https://hooks.slack.com/services/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", channel: '#development', membername: 'signup'
   #     notifier.ping "New Signup by " + params[:member][:name] + " email: " + params[:member][:email]
-  #   end 
+  #   end
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update) << :name
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.for(:account_update) << :name
+  # end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    # member_dashboard_path
-    redirect_to  new_team_path
-  end
+  # def after_sign_up_path_for(resource)
+  #   # member_dashboard_path
+  #   redirect_to  new_team_path
+  # end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
